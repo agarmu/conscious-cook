073a5fb17f334d01d5897be31687ca2ed37b9e87
hamesjan <60535502+hamesjan@users.noreply.github.com>
2020-04-11 15:55:10 -0700

    Update content.js

--- front-end/ConsciousCook/content.js
+++ front-end/ConsciousCook/content.js
@@ -1,1 +1,5 @@
-//alert("grr")
+
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    document.getElementById('settings_text').innerHTML = `<h2 id="settings_text">Curret recipe: ${tabs[0].title}</h2>`
+});
+
