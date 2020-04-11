aa69056cac82b07cfcd90fc00ed300abeb4c1c7c
hamesjan <60535502+hamesjan@users.noreply.github.com>
2020-04-11 15:54:19 -0700

    Delete content.js

--- content.js
+++
@@ -1,5 +0,0 @@
-
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    document.getElementById('settings_text').innerHTML = `<h2 id="settings_text">Curret recipe: ${tabs[0].title}</h2>`
-});
-
